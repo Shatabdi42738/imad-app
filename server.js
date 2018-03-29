2238d1256465941d49247582842ad1a85f65a85c
Shatabdi42738 <36434744+Shatabdi42738@users.noreply.github.com>
2018-03-29 19:55:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('Article two requested and is shown');
+ res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
